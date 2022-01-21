531b8f4310e98ea9651d068921231e51ae5d10ea
kweenkeen <40581664+kweenkeen@users.noreply.github.com>
2022-01-21 12:50:05 -0500

    Add files via upload
    Reads through ProofMode csv files, creates dictionary with field names, appends dictionary entry to correct fields in a new CSV file!

---
+++ create_csv_pandas_subdirectories_dictionary.py
@@ -0,0 +1,49 @@
+import csv
+from datetime import date
+import glob
+import os
+import pandas as pd
+
+# CREATE NEW CSV OF CONCATENATED PROOFS FROM EACH INDIVIDUAL CSV FILE
+
+todays_date = str(date.today())
+
+descriptor = input("Please enter a one word descriptor you would like to add to your filename. (If you would not like to add a descriptor, hit ENTER): ")
+
+newfilename = "ProofMode_Collection_" + todays_date + "_" + descriptor + ".csv"
+
+header = ['File Hash SHA256', 'Locale', 'SafetyCheckCtsMatch',
+       'Location.Provider', 'IPv6', 'IPv4', 'Location.Accuracy',
+       'Location.Latitude', 'Language', 'NetworkType', 'Network',
+       'Manufacturer', 'DataType', 'Hardware', 'ScreenSize', 'Wifi MAC',
+       'Notes', 'DeviceID', 'Location.Longitude', 'Location.Bearing',
+       'SafetyCheckBasicIntegrity', 'Location.Time', 'File Modified',
+       'CellInfo', 'SafetyCheck', 'Location.Altitude', 'SafetyCheckTimestamp',
+       'Proof Generated', 'File Path', 'Location.Speed', '']
+
+with open(newfilename, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(header)
+
+# READ IN FILES
+
+desired_date = input('What is the date for the files? Enter in YYYY-MM-DD format: ')
+name_of_download = input('Type in the exact name of the folder you downloaded: ')
+PATH = 'ProofModeFiles\\' + desired_date + '\\' + name_of_download
+EXT = '.csv'
+separator = ','
+
+## READ IN NEW PROOF
+
+from csv import reader
+
+for path, subdir, files in os.walk(PATH):
+    for proof_file in glob.glob(path + "\\*" + EXT):
+        current_proof = open(proof_file, newline='')
+        current_proof_dic = csv.DictReader(current_proof)
+        row2 = next(current_proof_dic)
+
+        with open(newfilename, 'a+', newline='') as csvfile:
+            header2 = current_proof_dic.fieldnames
+            csvdictwriter = csv.DictWriter(csvfile, fieldnames=header)
+            csvdictwriter.writerow(row2)
